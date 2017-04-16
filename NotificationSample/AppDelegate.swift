2022c74cfe2205818780043b6670b0c5cd77df96
Shusaku HARADA <higra-shi@dream.jp>
2017-04-16 19:58:15 +0900

    Call Local Notification

--- NotificationSample/AppDelegate.swift
+++ NotificationSample/AppDelegate.swift
@@ -176,7 +176,7 @@ extension AppDelegate : UNUserNotificationCenterDelegate {
         print(userInfo)
         
         // Change this to your preferred presentation option
-        completionHandler([])
+        completionHandler([.alert])
     }
     
     func userNotificationCenter(_ center: UNUserNotificationCenter,
--- NotificationSample/ViewController.swift
+++ NotificationSample/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
 
@@ -19,7 +20,32 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func currentDateString() -> String {
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "en_US_POSIX")
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        return formatter.string(from: Date())
+    }
 
+    @IBAction func OnTapNotificationButton(sender: UIButton) {
+        let dateString = currentDateString()
+        let notificationId = "Local-\(dateString.hashValue)"
+        
+        // 通知の内容を設定する
+        let content = UNMutableNotificationContent()
+        content.title = "Local"
+        content.body = dateString
+        content.userInfo = ["id": notificationId]
+        
+        // 通知の条件を設定する
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        
+        // 通知のリクエストを生成する
+        let request = UNNotificationRequest(identifier: notificationId, content: content, trigger: trigger)
+        // 通知をシステムに登録する
+        UNUserNotificationCenter.current().add(request)
+    }
 
 }
 
